263ef1967a8f5fbe9e93473f758409aadc221e60
AS-Brown <andrewsbrown@hotmail.co.uk>
2021-09-08 17:11:36 +0100

    Initial commit

---
+++ src/compare.py
@@ -0,0 +1,8 @@
+def compare(number_one, number_two):
+    if number_one < number_two:
+        return f"{number_one} is less than {number_two}"
+    elif number_one == number_two:
+        return f"{number_one} is the same as {number_two}"
+    elif number_one > number_two:
+        return f"{number_one} is greater than {number_two}"
+
